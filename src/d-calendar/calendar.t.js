4508e3cd75ea1419ea266e5d6ece9bdc0bc86f83
Pankaj Patel <patel.pankaj@live.in>
2017-08-18 23:14:40 +0200

    wip: rearrangement

--- src/d-calendar/calendar.t.js
+++ src/d-calendar/calendar.t.js
@@ -1,12 +1,9 @@
-module.exports = data => `<div class="js-component ${data.mode === data.modes.input ? 'hidden' : ''}">
-  ${data.mode === data.modes.input ? `<div class="arrow"></div>` : ''}
-  <div class="d-calendar-popup">
-    <div class="d-calendar animate">
-      <button type="button" class="d-calender-navigation-previous">&lArr;</button>
-      <button type="button" class="d-calender-navigation-next">&lArr;</button>
-      <div class="d-calendar-row">
-        ${data.months.map(month => data.monthTagTemplate(month)).join('')}
-      </div>
-    </div>
+module.exports = data => `
+<div class="d-calendar animate">
+  <button type="button" class="d-calender-navigation-previous">&lArr;</button>
+  <button type="button" class="d-calender-navigation-next">&lArr;</button>
+  <div class="d-calendar-row">
+    ${data.months.map(month => data.monthTagTemplate(month)).join('')}
   </div>
-</div>`;
+</div>
+`;
--- src/d-datepicker/datepicker.t.js
+++ src/d-datepicker/datepicker.t.js
@@ -3,3 +3,16 @@ module.exports = (data) => `
 ${data.attrs.map(item => `${item.name}="${item.value}"`).join(' ')}
 >${data.contents}</${data.config.calendarComponent}>
 `;
+
+module.exports = data => `<div class="js-component ${data.mode === data.modes.input ? 'hidden' : ''}">
+${data.mode === data.modes.input ? `<div class="arrow"></div>` : ''}
+<div class="d-calendar-popup">
+  <div class="d-calendar animate">
+    <button type="button" class="d-calender-navigation-previous">&lArr;</button>
+    <button type="button" class="d-calender-navigation-next">&lArr;</button>
+    <div class="d-calendar-row">
+      ${data.months.map(month => data.monthTagTemplate(month)).join('')}
+    </div>
+  </div>
+</div>
+</div>`;
