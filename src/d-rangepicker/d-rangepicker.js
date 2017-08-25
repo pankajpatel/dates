9cab2f8ab0ef5070cd945edf456f8cbca04b59d7
Pankaj Patel <patel.pankaj@live.in>
2017-08-25 08:13:35 +0200

    animate arrow on rangepicker

--- src/d-rangepicker/d-rangepicker.js
+++ src/d-rangepicker/d-rangepicker.js
@@ -11,25 +11,41 @@ import './d-rangepicker.scss';
 
 class RangePicker extends DatePicker {
   connectedCallback() {
+    this.mode = this.getAttribute('mode') || 'individual';
+
+    super.connectedCallback();
     // if (this.hasAttribute('step')) {
     //   attrs.push({ name: 'step', value: this.getAttribute('step') });
     // }
-    super.connectedCallback();
+    this.arrow = this.querySelector('.arrow');
+  }
+
+  removeFocus() {
+    // Hide calendar and remove focus ring
+    this._component.classList.add('hidden');
+    const focused = $find(`${this.input}.d-focused`, this)
+    if(focused.length > 0) {
+      focused.forEach(el => {
+        el.classList.remove('d-focused');
+      });
+    }
   }
 
   bindings() {
     this.calendar.addEventListener('change', () => {
       // Populate the input with picked value
       this.querySelector(`${this.input}.d-focused`).value = this.calendar.value;
-      // Hide calendar and remove focus ring
-      this._component.classList.add('hidden');
-      this.querySelector(`${this.input}.d-focused`).classList.remove('d-focused');
+      this.removeFocus();
     });
 
     $find(this.input, this).forEach((el) => {
       el.addEventListener(this.openEvent, (e) => {
+        this.removeFocus()
+        const input = e.target;
         this._component.classList.remove('hidden');
-        e.target.classList.add('d-focused');
+        input.classList.add('d-focused');
+        let position = input.getBoundingClientRect()
+        this.arrow.style.marginLeft = `${position.left + 10}px`;
         this.calendar.updateWidth();
       });
       // el.addEventListener(this.closeEvent, this.close)
